03bfe6e5c9dc35666c4c31eabfe1d2132a7b6d63
Ma Minh Quan <quan.ma@southasiaservices.com>
2022-06-22 14:29:28 +0700

    Modified index to start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mars = require("./mars");
 const earth = require("./earth");
 
-console.log("Init project");
+console.log("Start the journey!");
 console.log(mars.Mars() + "testing");
 console.log(earth.Earth() + "testing");
